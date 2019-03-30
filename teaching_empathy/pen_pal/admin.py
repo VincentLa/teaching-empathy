cf0621c6de5ee6837c8e54a1c77d96e2719ed2cf
Vincent La <vincent.la@cloverhealth.com>
2019-03-30 11:03:42 -0700

    registering models

--- teaching_empathy/pen_pal/admin.py
+++ teaching_empathy/pen_pal/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+import pen_pal.models as ppm
 
 # Register your models here.
+admin.site.register(ppm.Topic)
+admin.site.register(ppm.Literature)
+admin.site.register(ppm.TopicLiterature)
+admin.site.register(ppm.User)
+admin.site.register(ppm.Question)
+admin.site.register(ppm.Matches)
+admin.site.register(ppm.Conversation)
+admin.site.register(ppm.ConversationText)
+admin.site.register(ppm.Notification)
+admin.site.register(ppm.Survey)
+admin.site.register(ppm.Reports)
