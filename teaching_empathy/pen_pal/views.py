0a74a66b3c431f399a3b08eb715a1c22d6eeb289
Vincent La <vincent.la@cloverhealth.com>
2019-03-30 12:30:49 -0700

    Adding all updates and first home page

--- teaching_empathy/pen_pal/views.py
+++ teaching_empathy/pen_pal/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from pen_pal.models import User, Topic
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_users = User.objects.all().count()
+    num_topics = Topic.objects.all().count()
+    
+    # Users by Gender (gender = 'Male')
+    num_males = User.objects.filter(gender='Male').count()
+    num_females = User.objects.filter(gender='Female').count()
+    
+    context = {
+        'num_users': num_users,
+        'num_topics': num_topics,
+        'num_males': num_males,
+        'num_females': num_females,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
